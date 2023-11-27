311da561a6fc4d85c278be6c116bf9066f735826
Harshgupta2902 <147129512+Harshgupta2902@users.noreply.github.com>
2023-11-27 17:37:33 +0530

    routes

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -16,22 +16,6 @@ class Welcome extends CI_Controller {
         // $this->load->library('M_pdf');
 
     }
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
 	public function index()
 	{
 		$data['data'] = $this->db->query('select id,image,type from projects')->result_array();
